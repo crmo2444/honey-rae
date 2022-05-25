2e9d709a4b618f8c7db7e59305050c266a7221e1
Claire Morgan-Sanders <crmo2444@gmail.com>
2022-05-25 14:01:56 -0500

    added employee/customer views and search bar

---
+++ src/components/views/CustomerViews.js
@@ -0,0 +1,32 @@
+import { Outlet, Route, Routes } from "react-router-dom"
+import { TicketForm } from "../tickets/TicketForm"
+import { TicketList } from "../tickets/TicketList"
+import { TicketSearch } from "../tickets/TicketSearch"
+
+export const CustomerViews = () => {
+	return (
+        <Routes>
+            <Route path="/" element={
+                <>
+                    <h1>Honey Rae Repair Shop</h1>
+                    <div>Your one-stop-shop to get all your electronics fixed</div>
+
+                    <Outlet />
+                </>
+            }>
+
+                <Route path="tickets" element={ 
+                <>
+                    {/*These cannot communicate with one another until they are wrapped in a parent
+                    <TicketSearch />
+                    <TicketList /> 
+                    They will now be returned in TicketContainer.js
+                    */}
+                    <TicketList />
+                </>
+            } />
+				<Route path="ticket/create" element={ <TicketForm /> } />
+            </Route>
+        </Routes>
+    )
+}
---
+++ src/components/views/EmployeeViews.js
@@ -0,0 +1,33 @@
+import { Outlet, Route, Routes } from "react-router-dom"
+import { TicketContainer } from "../tickets/TicketContainer"
+import { TicketForm } from "../tickets/TicketForm"
+import { TicketList } from "../tickets/TicketList"
+import { TicketSearch } from "../tickets/TicketSearch"
+
+export const EmployeeViews = () => {
+	return (
+        <Routes>
+            <Route path="/" element={
+                <>
+                    <h1>Honey Rae Repair Shop</h1>
+                    <div>Your one-stop-shop to get all your electronics fixed</div>
+
+                    <Outlet />
+                </>
+            }>
+
+                <Route path="tickets" element={ 
+                <>
+                    {/*These cannot communicate with one another until they are wrapped in a parent
+                    <TicketSearch />
+                    <TicketList /> 
+                    They will now be returned in TicketContainer.js
+                    */}
+                    <TicketContainer />
+                </>
+            } />
+
+            </Route>
+        </Routes>
+    )
+}
