01b6d07963ce90d085fa52f39e564bfb80423a67
Claire Morgan-Sanders <crmo2444@gmail.com>
2022-05-27 10:00:40 -0500

    added edit ticket for customers

--- src/components/tickets/Ticket.js
+++ src/components/tickets/Ticket.js
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom"
 export const Ticket = ({id, description, emergency, date}) => {
     return <section className="ticket">
                     <div>
-                        <Link to={`/tickets/${id}`}>Ticket {id}:</Link>
+                        Ticket {id} 
+                        <br></br>   
+                    <Link to={`/tickets/${id}`}>(Details)</Link>
                     </div>
                     <div>Description: {description}</div>
                     <div>Emergency: {emergency ? "Yes" : "No"}</div>
+                    <Link to={`/tickets/${id}/edit`}>Edit</Link>
                 </section>
 }
